9685af9178c3738cde2a2ee8a5b1b6ec9aa32d60
kain <zhangtqx@vip.qq.com>
2020-03-31 15:29:55 +0800

    修正Factory

--- src/service/ObsService.php
+++ src/service/ObsService.php
@@ -3,7 +3,7 @@ declare (strict_types=1);
 
 namespace think\huaweicloud\extra\service;
 
-use think\aliyun\extra\common\OssFactory;
+use think\huaweicloud\extra\common\ObsFactory;
 use think\Service;
 
 class ObsService extends Service
@@ -13,7 +13,7 @@ class ObsService extends Service
         $this->app->bind('obs', function () {
             $config = $this->app->config
                 ->get('huaweicloud');
-            return new OssFactory($config);
+            return new ObsFactory($config);
         });
     }
 }
